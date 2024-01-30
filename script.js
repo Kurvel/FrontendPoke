389c3ee62d657ff358bbac06f64c483ee42523e8
Kurvel <143402591+Kurvel@users.noreply.github.com>
2024-01-30 17:54:12 +0100

    WIP GETfromBackend

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let Pokemon = document.getElementById("Pokemon");
 let pokemonList = document.getElementById("pokemonList");
 let pokemonInfo = document.getElementById("pokemonInfo");
 let pokemonAbilities = document.getElementById("pokemonAbilities");
+let pokiedexBtn = document.getElementById("pokiedexBtn");
+
+let pokemonUrl = "https://pokeapi.co/api/v2/pokemon/"
+let backendPokiedexUrl = "http://localhost:8080/pokemons"
 
 
 function fetchPokemons(url) {
@@ -11,8 +15,57 @@ function fetchPokemons(url) {
         
 }
 
+function fetchBackend(url) {
+    return fetch(url)
+        .then(res => res.json())
+        .then(data => printBackend(data)); 
+}
+
+fetchBackend(backendPokiedexUrl);
+
+function printBackend(backendData) {
+    
+    let backendList = backendData;
+    console.log(backendList);
+    backendList.map(pokemonId => {
+        console.log(pokemonId.id);
+
+        fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId.id}/`)
+       .then(res => res.json())
+       .then(pokemonData => console.log(pokemonData.name))
+        
+    })
+}
+
+ // pokemonList.innerHTML = "";
+    // let fetchPokemonUrl = fetch(pokemonUrl).then(res => res.json()); 
+    // let fetchBackendPokiedexUrl = fetch(backendPokiedexUrl).then(res => res.json());
+    // Promise.all([fetchPokemonUrl, fetchBackendPokiedexUrl]).then((data) => {
+        
+    //     let pokeApis = data[0].results;
+    //     console.log(pokeApis);
+    //     let pokeBackend = data[1];
+    //     console.log(pokeBackend);
+    
+    //     pokeApis.map(pokeApi => {
+    //         let li = document.createElement("li");
+    //         li.innerText = pokeApi.url;
+    //         pokemonList.appendChild(li);
+    //     })
+    // })
+
+
+
+
+pokiedexBtn.addEventListener("click", function () {
+   
+    
+        
+    });
+
+
 function printPokemons(pokemons) {
-    // console.log(pokemons);
+     console.log(pokemons);
     pokemonList.innerHTML = "";
     pokemons.forEach(pokemon => {
         let li = document.createElement("li");
@@ -72,4 +125,4 @@ function printPokemonInfo(pokemonData) {
 
 }
 
-fetchPokemons("https://pokeapi.co/api/v2/pokemon/")
+fetchPokemons(pokemonUrl)
